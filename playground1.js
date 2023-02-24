8f70ce1e5ad9fc6c8a8e6929f4b6ccb004411073
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-02-24 11:36:20 -0500

    playground has been solved

--- playground1.js
+++ playground1.js
@@ -47,13 +47,16 @@ class Banda {
       this.integrantes = [];
     }
     agregarIntegrante(integranteNuevo) {
-      //Debe validarse q no haya más de un baterista:
-        if (this.integrantes.find
-          (i => i.instrumento == 'Bateria')) {
-            console.log('No pueden haber 2 Bateristas')
-        } else {
-          this.integrantes.push(integranteNuevo);
-        } 
+      // Opción resuelta en conjunto con ChatGPT3:
+      const instrumentos = this.integrantes.map(integrante => integrante.instrumento); //crear un array con los instrumentos existentes en la banda
+      // La sintaxis "i => i.algo" es una fxn flecha (arrow function). Es una forma abreviada de declarar una fxn anónima. En el caso de esta explicación, la arrow function toma un argumento "i" y devuelve la propiedad "algo" del objeto "i"
+
+      if(!instrumentos.includes(integranteNuevo.instrumento)){ //agregar integrante nuevo siempre y cuando el instrumento que toque no esté contenido en el array de instrumentos de los integrantes que ya están en la banda
+        this.integrantes.push(integranteNuevo);
+        console.log(`${integranteNuevo.nombre} bienvenido a nuestra banda! Te esperamos para empezar a ensayar. No olvides trae tu propi@ ${integranteNuevo.instrumento}`);
+      } else {
+        console.log(`${integranteNuevo.nombre} lo sentimos, pero ya tenemos otra persona que toca ${integranteNuevo.instrumento}. Te avisaremos si te necesitamos en otro momento!`)
+      }
     }
   }
   
@@ -95,6 +98,20 @@ banda.agregarIntegrante(new Integrante({
   instrumento: "Bateria" 
 }))
 
+banda.agregarIntegrante(new Integrante({ 
+  nombre: "Ricky", 
+  instrumento: "Bajo" 
+}))
+
+banda.agregarIntegrante(new Integrante({ 
+  nombre: "Leo", 
+  instrumento: "Bateria" 
+}))
+
+banda.agregarIntegrante(new Integrante({ 
+  nombre: "Arnoldo", 
+  instrumento: "Flauta" 
+}))
 
 
 
@@ -106,3 +123,17 @@ banda.agregarIntegrante(new Integrante({
 // }
 // const banda2 = new Banda(data)
 
+
+
+
+
+
+
+// Consulta para ChatGPT3:
+// Crea una clase que se llame "Banda" al interior de la cual hay tres llaves: "nombre", "generos" e "integrantes". Tanto "generos" como "integrantes", son arrays vacíos.
+
+// Incluye un método dentro de la clase creada anteriormente, a través del cual pueda agregar un integrante a la banda. Ese método se debe llamar "agregarIntegrante" y lleva como argumento "integranteNuevo".
+
+// Crea una clase independiente de la anterior llamada "Integrante", a través de la cual pueda crear un objeto para cada integrante. Dicha clase debe tener las siguientes dos llaves: "nombre" e "instrumento".
+
+// Haz que antes de agregar un integrante al array de "integrantes", se verifique si el instrumento que toca el integrante que se quiere agregar ya está contenido en el array. Si el instrumento del integrante no hace parte del array, quiere decir que ese integrante puede añadirse; en cambio, si hay un integrante que tiene ese mismo instrumento, no se debe poder agregar el integrante que toca el mismo instrumento.
